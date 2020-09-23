e98c5a2755d26eb68ed2fd7b7b6b026d35d9865a
yokuny <felipe.vni@hotmail.com>
2020-09-23 14:39:55 -0300

    end and beggin of the canvas teleport

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ snake [0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right"
+let direction = "right"; //direção do movimento
 function criarBackground(){
     context.fillStyle = 'lightgreen'; //cada quadradinho verde
     context.fillRect(0, 0, 16 * box, 16 * box); //16 quadradinhos. cada quadradinho com 32 altura largura
@@ -17,7 +17,21 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+////////////////
+document.addEventListener('keydown', update); //ouça teclas do Document
+function update(event){ //muda a direção do movimento
+    if(event.keyCode == 37 && direction != "right") direction = "left"; //muda a direção caso a teclad clicado não seja a direção contraria a direção atual na hora do click
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+///////////////
 function iniciarJogo(){
+    if(snake[0].x > (15 * box) && direction == "right") snake[0].x = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].x < 0 && direction == "left") snake[0].x = (16 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].y > (15 * box) && direction == "down") snake[0].y = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].y < 0 && direction == "up") snake[0].y = (16 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    
     criarBackground();
     criarCobrinha();
     let snakeX = snake[0].x;
@@ -33,4 +47,4 @@ function iniciarJogo(){
     }
     snake.unshift(newHead);
 }
-let jogo = setInterval(iniciarJogo, 100); //roda a função a cada 100ms
+let jogo = setInterval(iniciarJogo, 150); //roda a função a cada 100ms
