a9c1c009da9b97418e077c1a074b118f584d0a12
yokuny <felipe.vni@hotmail.com>
2020-09-24 08:54:37 -0300

    acid effect :D

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let canvas = document.getElementById('snake'); //chamando apartir do id do eleme
 let context = canvas.getContext('2d'); //diz oque vai acontecer dentro do campo
 let box = 32; //32px cada quadradinho jogavel
 let food = {
-    x:Math.floor(Math.random() * 15 + 1) * box , //criando valor random para a posição
-    y:Math.floor(Math.random() * 15 + 1) * box 
+    x:Math.floor(Math.random() * 30 + 1) * box , //criando valor random para a posição
+    y:Math.floor(Math.random() * 30 + 1) * box 
 }
 let snake = [];
 snake [0] = {
@@ -12,20 +12,29 @@ snake [0] = {
 }
 let direction = "right"; //direção do movimento
 function criarBackground(){
-    context.fillStyle = 'lightgreen'; //cada quadradinho verde
-    context.fillRect(0, 0, 16 * box, 16 * box); //16 quadradinhos. cada quadradinho com 32 altura largura
+    context.fillStyle = gerarCor(); //cada quadradinho verde
+    context.fillRect(0, 0, 32 * box, 32 * box); //32 quadradinhos. cada quadradinho com 32 altura largura
+}
+function gerarCor(){
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const color = `rgb(${r},${g},${b})`;
+    return color;
 }
 function criarCobrinha(){
+    const cor = gerarCor()
     for(var i = 0; i < snake.length; i++){
-        context.fillStyle = 'green';
+        context.fillStyle = cor ;
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 function limitesCanvas(){
-    if(snake[0].x > (15 * box) && direction == "right") snake[0].x = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
-    if(snake[0].x < 0 && direction == "left") snake[0].x = (16 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
-    if(snake[0].y > (16 * box) && direction == "down") snake[0].y = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
-    if(snake[0].y < 0 && direction == "up") snake[0].y = (16 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].x > (32 * box) && direction == "right") snake[0].x = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].x < 0 && direction == "left") snake[0].x = (32 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].y > (31 * box) && direction == "down") snake[0].y = 0; //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
+    if(snake[0].y < 0 && direction == "up") snake[0].y = (32 * box); //se a posição no eixo x da cobra for maior que 15 posições o tamanho da caixa ela recebe 0 para voltar ao inicio
 }
 function movimentação(){
     let snakeX = snake[0].x;
@@ -37,8 +46,8 @@ function movimentação(){
     if(snakeX != food.x || snakeY != food.y){
         snake.pop(); //tira a ponta da cauda para fazer efeito de movimento
     }else{
-        food.x = Math.floor(Math.random() * 15 + 1) * box; //modifica valor da posição da fruta
-        food.y = Math.floor(Math.random() * 15 + 1) * box; 
+        food.x = Math.floor(Math.random() * 30 + 1) * box; //modifica valor da posição da fruta
+        food.y = Math.floor(Math.random() * 30 + 1) * box; 
     }
     let newHead = {
         x: snakeX,
@@ -47,7 +56,7 @@ function movimentação(){
     snake.unshift(newHead);
 }
 function drawFood(){
-    context.fillStyle = 'red'
+    context.fillStyle = gerarCor();
     context.fillRect(food.x, food.y, box, box); //preenchendo tal posição em tal altura e largura
 }
 ////////////////
@@ -59,6 +68,8 @@ function update(event){ //muda a direção do movimento
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 ///////////////
+
+
 function iniciarJogo(){
     criarBackground();
     criarCobrinha();
@@ -71,7 +82,11 @@ function iniciarJogo(){
             alert('Game Over');
         }
     }
+    let background = document.getElementById('backgroud');
+    background.style.backgroundColor = gerarCor();
+    let title = document.getElementById('title');
+    title.style.color = gerarCor();
 }
-let jogo = setInterval(iniciarJogo, 100); //roda a função a cada 100ms]
-
+let jogo = setInterval(iniciarJogo, 50); //roda a função a cada 100ms
+//mudar esse valor para meio random
 
